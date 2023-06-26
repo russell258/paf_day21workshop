d2b56cfd72f77fb230aba3bea922069965f00e49
russell258 <duckie258@gmail.com>
2023-06-26 16:51:18 +0800

    setup

---
+++ src/main/java/sg/edu/nus/iss/paf_day21workshop/PafDay21workshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_day21workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafDay21workshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafDay21workshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/paf_day21workshop/PafDay21workshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_day21workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafDay21workshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
